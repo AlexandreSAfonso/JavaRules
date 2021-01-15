5ad2400ba8d746955530da71e3c7f49f562a0047
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-01-15 17:24:11 -0300

    Add information input

---
+++ src/UserInformations.java
@@ -0,0 +1,26 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.Scanner;
+
+public class UserInformations {
+    public static void main(String[] args) {
+        Integer valor;
+
+        Scanner scanner  = new Scanner(System.in);
+        System.out.print("Informe um número: ");
+
+        valor = scanner.nextInt();
+        System.out.println(valor +10);
+
+/*
+        Double doubleValor = scanner.nextDouble();
+        Float floatValor = scanner.nextFloat();
+        Long longValor = scanner.nextLong();
+        Short shortvalor = scanner.nextShort();
+        Integer intValor = scanner.nextInt();
+        Byte bytevalor = scanner.nextByte();
+        Boolean boolvalor = scanner.hasNextBoolean()
+
+ */
+    }
+}
