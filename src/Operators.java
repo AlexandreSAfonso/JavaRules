5f30231cc0b83cb251f9eb1f80e7a942b834ab3f
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-01-15 17:11:34 -0300

    add number before and after

--- src/Operators.java
+++ src/Operators.java
@@ -3,10 +3,13 @@ public class Operators {
         Integer numero = 10;
 
         System.out.println(numero + 950);
-        
         System.out.println(numero - 89);
         System.out.println(numero * 2);
         System.out.println(numero / 3);
         System.out.println(numero % 3);
+
+        System.out.println(numero++); //utiliza (exibe) a variável e depois soma
+        System.out.println(++numero); //soma e depois utiliza (exibe)
+
     }
 }
