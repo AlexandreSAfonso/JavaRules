971012742e097a0bb06029e5c29707f1c6fa4783
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-01-15 16:45:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JavaRules
+ Curso da Udemy Android Iniciante https://www.udemy.com/course/desenvolvedor-android-iniciante
---
+++ src/OlaMundo.java
@@ -0,0 +1,12 @@
+public class OlaMundo {
+    /**
+     * Esse é um comentério de Documentação
+     * this is a document letter
+     */
+    //esse método é o ponto de entrada do programa em java
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo! "); /* primeiro
+                                                código
+                                                 executado */
+    }
+}
