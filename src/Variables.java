c958143a2d75320669e403259c8c2836b07ad986
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-01-15 16:52:00 -0300

    add file

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        /**
+         * Existem variáveis de vários tipos
+         * String
+         * Integer
+         *
+         * */
+
+        String nome = "Alexandre";
+        System.out.println(nome);
+    }
+}
