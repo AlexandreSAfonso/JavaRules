4512d9a28ec4c83bd6963f43108c6afd768aea1e
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-01-18 15:52:43 -0300

    update Variables String Study

--- src/VariablesStrings.java
+++ src/VariablesStrings.java
@@ -1,6 +1,21 @@
 public class VariablesStrings {
     public static void main(String[] args) {
         String nome = "Java";
+        //forma para encontrar um caractére na cadeia de string
+        System.out.println("Primeiro caractére: " + nome.charAt(2));
         
+        
+        //forma de concatenar
+        String sobrenome = "Rules!";
+        System.out.println(nome + " " + sobrenome);
+        
+        Integer idade  = 10;
+        System.out.println("Idade: " + idade);
+
+        //metodos auxiliares para conversão de valores
+        Double d = 1043860.9;
+        String strDouble = String.valueOf(d);
+        System.out.println(strDouble);
+
     }
 }
