b459d06459f630c934c3b9502b18938286e8696f
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-01-18 13:54:20 -0300

    Update VariablesStrings.java

--- src/VariablesStrings.java
+++ src/VariablesStrings.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class VariablesStrings {
+public class VariablesStrings {
+    public static void main(String[] args) {
+        String nome = "Java";
+        
+    }
 }
