d94b490ca8256dffcd623df3efcef0fb1ddddbcb
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-01-18 15:57:10 -0300

    Create TiposPrimitivos.java

---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //tipo primitivo não tem método
+        int a1 = 10;
+
+        //tipos não primitivos contém métodos
+        Integer a2 = 10;
+
+        /**
+         * Double   = double
+         * Float    = float
+         * Long     = long
+         * Integer  = int
+         * Short    = short
+         * Byte     = byte
+         * Boolean  = boolean
+         * Character= char
+         *
+         * O String não tem tipo primitivo 
+         * */
+
+    }
+}
